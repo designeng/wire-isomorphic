43ddb3ad2e99db8992d5aae705d0f3ce819a49ec
designeng <denissavenok@gmail.com>
2016-07-11 20:08:54 +0300

    code cleaned

--- src/tasks/bootstrap/getCarcassFn.js
+++ src/tasks/bootstrap/getCarcassFn.js
@@ -1,25 +1,7 @@
 // import carcass from '../../templates/build/carcass';
 
-const headerHtml = (cities, currentCity, receptionButtons, specialTitleData, requestUrl) => {
-    return header({
-        topControls: topControls({
-            receptionButtons,
-            specialTitleData
-        }),
-        logo: logo(),
-        citySelector: citySelector({
-            // TODO: take it from page cookie?
-            currentCity: currentCity ? currentCity.name : 'Москва',
-            cities
-        }),
-        nav: nav({
-            request: requestUrl
-        })
-    })
-};
-
 function getCarcassFn(brands, cities, requestUrl) {
-    
+
 }
 
 export default getCarcassFn;
