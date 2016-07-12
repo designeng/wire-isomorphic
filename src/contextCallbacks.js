f44f7422c8602dc698f4363d9a20b156fcefc990
designeng <denissavenok@gmail.com>
2016-07-12 12:41:51 +0300

    cleaned code

--- src/contextCallbacks.js
+++ src/contextCallbacks.js
@@ -2,15 +2,9 @@ import _ from 'underscore';
 import showNotFoundPage from './utils/express/showNotFoundPage';
 import Timer from './utils/timer';
 
-import { replaceWidgetPlaceholder } from './utils/widgets/replacement';
-
-// TODO. The post route returns json - not html.
-// fix variable name in all tasks specs: body{ html } -> body{ content }, e.g.
-
 export function success(request, response) {
     return (context) => {
         let html;
-        // console.log("url, specName:::", context.routeUrl, context.specName);
         
         let headers = {'Referer': `https://www.drive.ru${request.url}`}
         if(context.headers) {
