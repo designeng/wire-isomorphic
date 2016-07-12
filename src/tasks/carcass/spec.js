31f762697c3dfd2bd09e4cff8cbb7d25fa02b4ce
designeng <denissavenok@gmail.com>
2016-07-12 23:32:22 +0300

    StyleSheets & BottomScripts components

--- src/tasks/carcass/spec.js
+++ src/tasks/carcass/spec.js
@@ -8,6 +8,8 @@ import webpackCompilePlugin from '../../plugins/webpack'
 import { getEndpoint, getLocalEndpoint } from '../../config/api';
 
 import carcassTpl from '../../templates/build/carcassTpl';
+import styleSheetsTpl from '../../templates/build/styleSheetsTpl';
+import bottomScriptsTpl from '../../templates/build/bottomScriptsTpl';
 import axisTpl from '../../templates/build/axisTpl';
 import axisCellTpl from '../../templates/build/axisCellTpl';
 
@@ -61,24 +63,42 @@ export default {
         }
     },
 
-    carcassData: {
-        styleSheets: [
+    styleSheetsData: {
+        items: [
             {path: '/css/carcass.css'}
-        ],
-        bottomScripts: [
+        ]
+    },
+
+    styleSheets: {
+        createComponent: {
+            template: styleSheetsTpl,
+            datasource: {$ref: 'styleSheetsData'},
+        }
+    },
+
+    bottomScriptsData: {
+        items: [
             {sync: true, src: '/js/main.js'},
             {sync: true, src: {$ref: 'compiledScript'}}
-        ]
+        ],
+    },
+
+    bottomScripts: {
+        createComponent: {
+            template: bottomScriptsTpl,
+            datasource: {$ref: 'bottomScriptsData'},
+        }
     },
 
     carcass: {
         createComponent: {
             template: carcassTpl,
-            datasource: {$ref: 'carcassData'},
             tags: [
+                {StyleSheets: {$ref: 'styleSheets'}},
                 {AxisX: {$ref: 'axisX'}},
                 {AxisY: {$ref: 'axisY'}},
                 {PageContent: {$ref: 'pageContent'}},
+                {BottomScripts: {$ref: 'bottomScripts'}},
             ]
         }
     }
