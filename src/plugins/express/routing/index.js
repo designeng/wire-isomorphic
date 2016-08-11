af9b2f107ab0720fa0157c57a56034da056b34b8
designeng <denissavenok@gmail.com>
2016-08-11 16:07:31 +0300

    || equivalent to if

--- src/plugins/express/routing/index.js
+++ src/plugins/express/routing/index.js
@@ -15,15 +15,14 @@ import pluck from '../../../utils/pluck';
 let registeredUrlPlugins = {};
 
 function registerRoutePlugins(url, routeSpecs, specs) {
-    registeredUrlPlugins[url] ? void 0 : registeredUrlPlugins[url] = {
+    registeredUrlPlugins[url] || (registeredUrlPlugins[url] = {
         plugins: [],
         names: {}
-    };
+    });
 
     _.each(routeSpecs, (spec) => {
         _.each(specs[spec].$plugins, (plugin) => {
-            registeredUrlPlugins[url].names[plugin.name] ? void 0 
-                : registeredUrlPlugins[url].names[plugin.name] = 1 && registeredUrlPlugins[url].plugins.push(plugin);
+            registeredUrlPlugins[url].names[plugin.name] || (registeredUrlPlugins[url].names[plugin.name] = 1 && registeredUrlPlugins[url].plugins.push(plugin));
         });
     });
 }
