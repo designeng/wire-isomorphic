1af17dbb6b22af9a9fcb1bc0086f72b7dfc7a581
designeng <denissavenok@gmail.com>
2016-08-31 12:31:39 +0300

    minor

--- src/permissions.js
+++ src/permissions.js
@@ -1,14 +1,14 @@
 const permissions = [
     {
-        roles:['guest', 'member'],
-        allows:[
-            {resources: 'blogs', permissions:['takeALook']},
-            {resources: 'forums', permissions:['get', 'put','delete', 'talkAbout']},
-            {resources: 'news', permissions:['make', 'inspect', 'accept']},
+        roles: ['guest', 'member'],
+        allows: [
+            {resources: 'blogs', permissions: ['takeALook']},
+            {resources: 'forums', permissions: ['get', 'put','delete', 'talkAbout']},
+            {resources: 'news', permissions: ['make', 'inspect', 'accept']},
         ]
     },
     {
-        roles:['gold', 'silver'],
+        roles: ['gold', 'silver'],
         allows:[
             {resources: 'cash', permissions: ['sell','exchange']},
             {resources: ['account','deposit'], permissions: ['put','delete']}
