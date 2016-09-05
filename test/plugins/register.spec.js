b71c13214e0fadcc66746fc0a68c8ba8616520bb
designeng <denissavenok@gmail.com>
2016-09-05 15:43:49 +0300

    register.spec inited

---
+++ test/plugins/register.spec.js
@@ -0,0 +1,14 @@
+import chai, { expect } from 'chai';
+
+import { preprocessPosts } from '../../src/pages/posts/preprocessors';
+
+describe('register plugins', () => {
+
+    it('should ..... ', () => {
+        expect().to.equal();
+    });
+
+    it('should ...... ', () => {
+        expect().to.equal();
+    });
+});
