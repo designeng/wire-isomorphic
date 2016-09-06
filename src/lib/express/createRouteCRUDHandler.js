d8eb35db511e176ea530cb6185aafac14ff883fd
designeng <denissavenok@gmail.com>
2016-09-06 21:44:47 +0300

    cleaned code

--- src/lib/express/createRouteCRUDHandler.js
+++ src/lib/express/createRouteCRUDHandler.js
@@ -2,13 +2,11 @@ import crud from 'node-crud';
 
 const crudActions = ['Create', 'Read', 'Update', 'Delete'];
 
-let launched = false;
-
 export default function createRouteCRUDHandler(target, url) {
     crudActions.forEach((action) => {
         crud.entity('/fakecomments')[action]()
             .pipe(function(data, query, cb) {
-                cb(null, [ { name: 'bobby tables' } ]);
+                cb(null, [ { action: action } ]);
             });
     });
 }
