275eaa9bdc9e1eed8e139caa5b6ededc54143e36
designeng <denissavenok@gmail.com>
2016-09-13 13:46:09 +0300

    imstalled bcrypt; UserSchema added comparePassword method; added pre save hook

--- src/modules/users/entities/User.js
+++ src/modules/users/entities/User.js
@@ -1,6 +1,9 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 
-let schema = new Schema({
+const SALT_WORK_FACTOR = 10;
+
+let schema = new UserSchema({
     name: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true },
     created: { type: Date, default: Date.now },
@@ -9,6 +12,34 @@ let schema = new Schema({
     status: Boolean,
     role_id: String,
 });
+
+UserSchema.pre('save', (next) => {
+    let user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 let User = mongoose.model('User', schema);
 
