a3acf51b3d2b0e55e0eeb460e5296ca8d7753f34
designeng <denissavenok@gmail.com>
2016-09-13 16:25:49 +0300

    auth handler works

--- src/tasks/api/service/auth/index.js
+++ src/tasks/api/service/auth/index.js
@@ -2,25 +2,25 @@ import User from '../../../../modules/users/entities/User';
 
 export default function auth(route) {
     return function(request, response, next) {
+        response.writeHead(200, route.headers);
 
         let username = request.body.username;
         let password = request.body.password;
 
-        User.findOne({ username }, function(err, user) {
-            if (err) throw err;
-
-            // test a matching password
-            user.comparePassword(password, function(err, isMatch) {
-                if (err) throw err;
-                response.writeHead(200, route.headers);
-                response.end(JSON.stringify({ res: isMatch }));
-            });
-
-            // // test a failing password
-            // user.comparePassword('123Password', function(err, isMatch) {
-            //     if (err) throw err;
-            //     console.log('123Password:', isMatch); // -> 123Password: false
-            // });
+        User.find({ username }, function(err, result) {
+            if (err) {
+                return response.end(JSON.stringify({ ERROR: err }));
+            } else {
+                let user = result[0];
+                if(user) {
+                    user.comparePassword(password, function(err, isMatch) {
+                        if (err) throw err;
+                        response.end(JSON.stringify({ res: isMatch }));
+                    });
+                } else {
+                    response.end(JSON.stringify({ res: `No such user with username ${username}` }));
+                }
+            }
         });
     }
 }
