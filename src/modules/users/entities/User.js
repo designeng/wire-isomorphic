d8cb290152866c13fbfc4b47ca929fe6eb7cdd98
designeng <denissavenok@gmail.com>
2016-09-13 13:40:13 +0300

    user schema name, password fields

--- src/modules/users/entities/User.js
+++ src/modules/users/entities/User.js
@@ -1,8 +1,8 @@
 import mongoose, { Schema } from 'mongoose';
 
 let schema = new Schema({
-    name:    String,
-    pass:  String,
+    name: { type: String, required: true, index: { unique: true } },
+    password: { type: String, required: true },
     created: { type: Date, default: Date.now },
     access: { type: Date, default: Date.now },
     login: { type: Date, default: Date.now },
