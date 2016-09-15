55aae2cf44463cd015d337a375d6449bb5945ca6
designeng <denissavenok@gmail.com>
2016-09-15 11:50:57 +0300

    drop database promisified

--- scripts/dropDatabase.js
+++ scripts/dropDatabase.js
@@ -1,19 +1,32 @@
-var MongoClient = require('mongodb').MongoClient;
+import when from 'when';
+import pipeline from 'when/pipeline';
 
-var url = 'mongodb://localhost:27017/isomorphic_dev';
+import { MongoClient } from 'mongodb';
 
-MongoClient.connect(url, function(err, db) {
-    if(err) {
-        console.log('ERROR:', err);
-    } else {
+let url = 'mongodb://localhost:27017/isomorphic_dev';
+let Promise = when.promise;
+
+const connectP = () => {
+    return Promise((resolve, reject) => {
+        MongoClient.connect(url, function(err, db) {
+            resolve(db);
+        });
+    });
+}
+
+const dropDatabaseP = (db) => {
+    return Promise((resolve, reject) => {
         db.dropDatabase(function(err, res) {
             console.log('Dropped isomorphic_dev');
-            process.exit();
+            resolve(db);
         });
+    });
+}
 
-        db.createCollection('users', function(err, res) {
-            console.log('created collection');
-            process.exit();
-        })
-    }
-});
+const run = () => {
+    pipeline([connectP, dropDatabaseP]).then((res) => {
+        console.log('task end');
+    });
+}
+
+run();
