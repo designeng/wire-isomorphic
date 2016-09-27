27124439301000d2b02032101fff9e8dde33071b
designeng <denissavenok@gmail.com>
2016-09-27 13:41:24 +0300

    Render summary

--- src/tasks/comments/spec.js
+++ src/tasks/comments/spec.js
@@ -1,5 +1,8 @@
 import wireDebugPlugin from 'essential-wire/source/debug';
 import vidomComponentPlugin from '../../plugins/component/vidom';
+import requestPlugin from '../../plugins/api/request';
+
+import { getEndpoint, getLocalEndpoint } from '../../config/api';
 
 import commentFormTpl from '../../templates/forms/commentFormTpl';
 import commentsListTpl from '../../templates/commentsListTpl';
@@ -10,10 +13,15 @@ import { client, server } from '../../decorators/environment';
 export default {
     $plugins: [
         // wireDebugPlugin,
+        requestPlugin,
         vidomComponentPlugin
     ],
 
-    commentsListData: [{body: 'first comment'}, {body: 'second comment'}],
+    commentsListData: {
+        request: {
+            endpoint: getLocalEndpoint('/api/v1/comments'),
+        }
+    },
 
     commentsList: {
         createComponent: {
--- src/templates/commentsListTpl.js
+++ src/templates/commentsListTpl.js
@@ -1,7 +1,7 @@
 import _ from 'underscore';
 const view = (items) => {
     let _items = _.map(items, (item) => {
-        return <li>{item.body}</li>
+        return <li>{item.title}</li>
     });
     return <ul>{_items}</ul>
 }
