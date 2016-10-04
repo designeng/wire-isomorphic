5d51dcadc0168d6c4ee030195b1e2d6b5b2851da
designeng <denissavenok@gmail.com>
2016-10-04 13:18:13 +0300

    Remove tag condition

--- src/plugins/component/snabbdom/index.js
+++ src/plugins/component/snabbdom/index.js
@@ -7,36 +7,13 @@ let env = process.env.ENVIRONMENT;
 function createComponent(resolver, compDef, wire) {
     wire(compDef.options).then(({
         template,
-        tags,
         datasource = {}
     }) => {
-
-        if(tags) {
-            if(!_.isArray(tags)) throw new TypeError('[createComponentPlugin:] tags option should be an array!');
-
-            let html = template(datasource);
-
-            if(tags.length) {
-                let tagsList = _.map(tags, (item) => {
-                    let name = _.keys(item)[0];
-                    return {
-                        name,
-                        html: item[name]
-                    }
-                });
-
-                _.each(tagsList, (tag) => {
-                    let tagRegex = new RegExp(`<${tag.name}(\\s+)\/>`, 'g');
-                    html = html.replace(tagRegex, tag.html);
-                });
-            }
+        if(env === 'server') {
+            const html = toHTML(template(datasource));
             resolver.resolve(html);
-
         } else {
-            if(env === 'server') {
-                const html = toHTML(template(datasource));
-                resolver.resolve(html);
-            }
+            // TODO: client
         }
     });
 }
