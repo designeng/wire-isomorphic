f51d0e996097d3765a94d635e3f5dbb1289f1afb
designeng <denissavenok@gmail.com>
2016-10-07 01:38:31 +0300

    Trace result

--- src/templates/snabbdomBottomScriptsTpl.js
+++ src/templates/snabbdomBottomScriptsTpl.js
@@ -4,13 +4,15 @@ import hh from 'hyperscript-helpers';
 const { script } = hh(h);
 
 export const snabbdomBottomScriptsTpl = (data) => {
-    return script('', {
-        attrs: {
+    let result = script('', {
+        props: {
             type: 'text/javascript',
             async: true,
             src: data.src
         }
     })
+    console.log(result);
+    return result;
 }
 
 export default snabbdomBottomScriptsTpl;
